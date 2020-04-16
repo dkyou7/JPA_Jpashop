77412224739e5d2a59dafad0e32e39742215f152
youdongkwan <dkyou7@naver.com>
2020-04-16 09:42:23 +0900

    에러난 상태로 제출

--- src/main/java/jpabook/jpashop3/Member.java
+++ src/main/java/jpabook/jpashop3/Member.java
@@ -1,7 +1,17 @@
 package jpabook.jpashop3;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
+@Getter @Setter
 public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
 }
